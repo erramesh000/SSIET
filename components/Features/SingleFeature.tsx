0c4dbf8d8f3751e1c0e65db24c8d6a000b602fe6
erramesh000 <erramesh000@gmail.com>
2025-05-13 21:49:59 +0530

    update wats app link

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -38,6 +38,24 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
           {subtitle}
         </h4>
         <p className="cousec">{description}</p>
+        <div>
+          <a
+            href="https://wa.me/8558831009"
+            className="group mt-7.5 inline-flex items-center gap-2.5 text-black hover:text-primary dark:text-white dark:hover:text-primary" target="_blank"
+          >
+            <span className="duration-300 group-hover:pr-2">
+              Know More
+            </span>
+            <svg
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+              fill="currentColor"
+            >
+              <path d="M10.4767 6.16701L6.00668 1.69701L7.18501 0.518677L13.6667 7.00034L7.18501 13.482L6.00668 12.3037L10.4767 7.83368H0.333344V6.16701H10.4767Z" />
+            </svg>
+          </a>
+        </div>
       </motion.div>
     </>
   );
