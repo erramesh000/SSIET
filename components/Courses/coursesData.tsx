492e4a5582ddf9dd7f26fc5c68f9f89f7f43dd05
erramesh000 <erramesh000@gmail.com>
2025-05-22 06:26:03 +0530

    updated watsapp

--- components/Courses/coursesData.tsx
+++ components/Courses/coursesData.tsx
@@ -82,7 +82,7 @@ export const coursesDataM: Courses[] = [
     subtitle: "3 Years / 6 Semesters",
     description:
       "All those candidates who have passed 10+2 in any stream or equivalent examination from any Board recognised or established by Central / State Government through a legislation and member of Council of School Education (COBSE) as equivalent thereto with at least : 33% marks in the aggregate in case of commerce students who have passed 10+2 examination OR 40% for the students who have opted for two of the following subjects in 10+2  (a)	Accountancy  (b) Business Studies © Economics (d) Mathematics (e) Office Management and Secretarial Practice (f) Principle and Practice of Insurance (g) Tax Procedure and Practice OR 45% Marks for other candidates.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Computer%20Applications%20(BCA).%20Please%20share%20more%20details.",
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Commerce%20-%20Honours.%20Please%20share%20more%20details.",
   },
   {
     id: 3,
@@ -91,7 +91,7 @@ export const coursesDataM: Courses[] = [
     subtitle: "4 Years / 8 Semesters",
     description:
       "All those candidates who have passed 10+2 or its equivalent examination with at least 45% marks (40% mark in case of candidate belonging to reserved category) in any stream from any Board recognised or established by Central / State Government through a legislation and member of Council of School Education (CBSE).          OR Passed D.Voc. Stream in the same or allied sector.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Computer%20Applications%20%E2%80%93%20Lateral%20Entry.%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Hotel%20Management%20%26%20Catering%20Technology.%20Please%20share%20more%20details."
   },
   {
     id: 4,
@@ -106,7 +106,7 @@ Administration
     subtitle: "2 Years / 4 Semesters",
     description:
       "All those candidates who have passed Bachelor degree of 3 years  with at least 50% marks (45% marks in case of candidate belonging to reserved category) in the qualifying examination from any University recognized by the UGC. ",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20in%20Business%20Administration%20(MBA)%20with%20specializations%20in%20HRM%2C%20Finance%2C%20Marketing%2C%20and%20IT.%20Please%20share%20more%20details."
   },
   {
     id: 5,
@@ -115,7 +115,7 @@ Administration
     subtitle: "2 Years / 4 Semesters",
     description:
       "All those candidates who have passed any graduation in Commerce (. B.E. / B. Tech. / B.Sc. / B. Com. / B.A. / B. Voc. / BCA etc) with at least 50% marks (45% marks in case of candidate belonging to reserve category) preferably with Mathematics at l0+2 I Graduation level from any University recognised by UGC.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Computer%20Applications%20(MCA).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Commerce.%20Please%20share%20more%20details."
   },
   {
     id: 6,
@@ -124,7 +124,7 @@ Administration
     subtitle: "3 Years/ 6 Semesters",
     description:
       "All those candidates who have passed any bachelors degree with minimum three year duration along with Mathematics / Statistics / Business Mathematics / Business Statistics / Quantitative Techniques / Computer Science / Information Technology as Compulsory / Optional / Additional Subjects either at 10+2 or graduation level OR PGDCA / BCA / B.Sc. (IT)  level from any University recognised by UGC.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Vocation%20(Fashion%20Design%20and%20Garment%20Technology).%20Please%20share%20more%20details."
   },
 
 ];
@@ -136,7 +136,7 @@ export const coursesDataScience: Courses[] = [
     subtitle: "3 Years / 6 Semesters",
     description:
       "All those candidates who have passed 10+2 or equivalent examination with Physics, Chemistry and Mathematics as compulsory subjects with 50%marks in aggregate (45% marks in case of candidate belonging to reserved category) from any Board recognized or established by Central / State Government through a legislation and member of Council of School Education.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Business%20Administration.%20Please%20share%20more%20details.",
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20(Non-Medical).%20Please%20share%20more%20details.",
   },
   {
     id: 2,
@@ -146,7 +146,7 @@ export const coursesDataScience: Courses[] = [
     description:
       `All those candidates who have passed 10+2 in
  Non Medical (Physics, Chemistry and Mathematics as compulsory subjects) or equivalent examination from any Board recognized or established by Central / State Government through a legislation and member of Council of School Education (COBSE).                	OR Three years Diploma (after Matriculation Examination) in CSE / IT from any recognized State Board of Technical Education or Sant Longowal Institute of ngineering & Technology, Longowal.`,
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Computer%20Applications%20(BCA).%20Please%20share%20more%20details.",
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20(Computer%20Science).%20Please%20share%20more%20details.",
   },
   {
     id: 3,
@@ -161,7 +161,7 @@ export const coursesDataScience: Courses[] = [
  and also obtained minimum 50% marks in aggregate (45% marks in case of candidate belonging to reserved category) in qualifying examination from recognized University / Board recognized or established by Central / State Government through a legislation and member of Council of School Education (COBSE)           OR
 Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharmacy after 10th/12th, with 50%marks in aggregate (45%marks in case of candidate belonging to reserved category) in qualifying examination from a recognized University / board/ council.
 `,
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Computer%20Applications%20%E2%80%93%20Lateral%20Entry.%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20(Medical%20Lab%20Science).%20Please%20share%20more%20details."
   },
   {
     id: 4,
@@ -170,7 +170,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "2 Years / 4 Semesters",
     description:
       "All those candidates who have passed Graduate degree or equivalent with minimum 50% marks (45% marks in case of candidate belonging to reserved category) in aggregate with Chemistry as one of the subjects from any University recognized by UGC.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Chemistry).%20Please%20share%20more%20details."
   },
   {
     id: 5,
@@ -179,7 +179,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "2 Years / 4 Semesters",
     description:
       "All those Candidates who have passed B.sc with Physics and mathematics as compulsory subject with at least 50% marks in aggregate (45% marks in case of candidate belonging to reserved category) from a University recognized by UGC.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Computer%20Applications%20(MCA).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Physics).%20Please%20share%20more%20details."
   },
   {
     id: 6,
@@ -188,7 +188,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "2 Years / 4 Semesters",
     description:
       "All those Candidates who have passed B.A/B.sc or equivalent degree from a recognized university with mathematics as one of the major subjects with at least 50% marks in aggregate. Relaxation of the minimum marks to reserved category is as per Punjab government notified rules.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Mathematics).%20Please%20share%20more%20details."
   },
   {
     id: 7,
@@ -197,7 +197,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "3 Years / 6 Semesters (including 1 year OR 2 semesters of Internship)",
     description:
       "AII those candidates who have passed minimum 2 Years Diploma in Medical Lab Sciences /Medical Lab Technology / Pharmacy after 10+2 with Medical /Non Medical stream or equivalent examination with Physics. Chemistry as Compulsory subjects and any of the following subject(s) in addition: (a) Biology (b) Mathematics (c) Botany & Zoology together and also obtained minimum 50% marks in aggregate (45% marks in case of candidate belonging to reserved category) in qualifying examination from the University recognized by UGC / Council / Board recognized or established by Central / State Government through legislation. OR Minimum 2 Years Diploma in Medical Lab Assistant after 10+2 with Medical / Non-Medical stream or equivalent examination with Physics, Chemistry as Compulsory subjects and any of the following subject(s) in addition: (a) Biology (b) Mathematics(c) Botany & Zoology together and also obtained minimum 50% marks in aggregate (45%marks in case of candidate belonging to reserved category) in qualifying examination from J&K Para Medical and Nursing Council or such examination from any recognized State Board of Technical Education.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20(Medical%20Lab%20Sciences)%20-%20Lateral%20Entry.%20Please%20share%20more%20details."
   },
   {
     id: 8,
@@ -206,7 +206,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "4 Years / 8 Semesters",
     description:
       "All those candidates who have passed 10+2 or equivalent examination with Physics, Chemistry as compulsory subjects along with either Mathematics or Biology or Agriculture as one of the additional subjects with at lea$ 50% marks in aggregate (45% marks in case of candidate belonging to reserved category) from any Board recognised or established by Central / State Government through a legislation and member of Council of School Education (COBSE).",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Bachelor%20of%20Science%20-%20Honors%20(Agriculture).%20Please%20share%20more%20details."
   },
   {
     id: 9,
@@ -215,7 +215,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "4 Years / 8 Semesters",
     description:
       "All those candidates who have passed 10+2 examination from any Board recognised or established by Central / State Government through a legislation and member of C-ouncil of School Education (COBSE) as equivalent to 10+2 examination by the Association of Indian Universities with English as one of the subject and Physics, Chemistry, Mathematics / Biology as optional subjects individually. Candidate should complete the age of 17 years on or before 31st December of the year of admission to the course.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20B.Pharmacy.%20Please%20share%20more%20details."
   },
   {
     id: 10,
@@ -224,7 +224,7 @@ Minimum 2 Years Diploma in Medical Lab Sciences / Medical Lab Technology / Pharm
     subtitle: "4 Years / 8 Semesters",
     description:
       "All those candidates who have passed 10+2 examination from any Board recognised or established by Central / State Government through a legislation and member of C-ouncil of School Education (COBSE) as equivalent to 10+2 examination by the Association of Indian Universities with English as one of the subject and Physics, Chemistry, Mathematics / Biology as optional subjects individually. Candidate should complete the age of 17 years on or before 31st December of the year of admission to the course.",
-    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20Master%20of%20Science%20(Information%20Technology).%20Please%20share%20more%20details."
+    linkk: "https://wa.me/918558831009?text=Hello%2C%20I%20am%20interested%20in%20the%20D.Pharmacy.%20Please%20share%20more%20details."
   },
 ];
 //export default coursesData; 
